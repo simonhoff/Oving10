09cb4050f21d646480828a74e07e2b9f1bf81e54
simonhoff <simonaho@stud.ntnu.no>
2016-03-13 21:29:55 +0100

    Completed Image class and added abstract shape class

--- Image.cpp
+++ Image.cpp
@@ -8,14 +8,11 @@
  * all the functions in this file.                   *
  *****************************************************/
 
-Image::Image( unsigned int width, unsigned int height ) {
+Image::Image( unsigned int width, unsigned int height ):width(width), height(height) {
    data = new unsigned char*[height];
    for (int row = 0; row < height; row++ ){
    		*(data + row) = new unsigned char[width * 3];
-   		}
    	}
-   	this->height = height;
-   	this->width = width;
 }
 
 Image::~Image()  {
@@ -33,26 +30,33 @@ unsigned int Image::getHeight() const  {
    return this->height;
 }
 
-const unsigned char * Image::getScanLine(unsigned int line) const  {
+const unsigned char * Image::getScanLine(unsigned int line) const  {	
    /* Enter your code here */
-   return (data + line);
+   if (line < height) return *(data + line);
 }
 unsigned char * Image::getScanLine(unsigned int line) {
-   return (data + line);
+   if (line < height) return *(data + line);
 }
 
 Color Image::getColor( unsigned int x, unsigned int y ) const {
-   struct Color retColor();
-   retColor.r = *(*(data + y) + x*3);
-   retColor.g = *(*(data + y) + x*3 + 1);
-   retColor.b = *(*(data + y) + x*3 + 2);
-   return retColor;
+	if (x < width && y < height){   
+	   struct Color retColor;
+   		retColor.red = *(*(data + y) + x*3);
+	   	retColor.green = *(*(data + y) + x*3 + 1);
+   		retColor.blue = *(*(data + y) + x*3 + 2);
+   		return retColor;
+   	}else{
+   		struct Color col;
+   		return col;
+   	}
 }
 void Image::setColor( unsigned int x, unsigned int y, const Color &color ) {
-	unsigned char address = *(*(data + y) + 3*x);
-	*(address++) = color.r;
-	*(address++) = color.g;
-	*(address) = color.b;
+	if (x < width && y < height){
+		unsigned char* address = *(data + y) + 3*x;
+		*(address++) = color.red;
+		*(address++) = color.green;
+		*(address) = color.blue;
+	}
 }
 
 void Image::fill( const Color &color ) {
--- tests.cpp
+++ tests.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <cstdlib>
 #include "Image.h"
-#include "ImageIO.cpp"
 
 int main(){
-	Image solidColor(200,300);
-	Image point(200,300);
-	solidColor.fill(struct Color col(193,84,193));
-	point.setColor(10,10,struct Color clr(200,0,0));
+	Image solidColor(200,500);
+	Image point(200,500);
+
+	struct Color col(193, 84, 193);
+	struct Color fill(0,0,200);
+	struct Color clr(200, 0, 0);
+
+	solidColor.fill(col);
+	point.fill(fill);
+	point.setColor(0,10, clr);
+	point.setColor(1,10, clr);
+	point.setColor(0,11, clr);
+	point.setColor(1,11, clr);
 	int success = saveImage(solidColor, "solid");
 	success = saveImage(point, "point");
 	return 0;
